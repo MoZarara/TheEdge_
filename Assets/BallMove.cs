34e028eb6b2f1443f60d353ae66955adf0031f40
MoZarara <no_name20017@yahoo.com>
2022-02-23 20:49:49 +0200

    no message

---
+++ Assets/BallMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    Rigidbody rig;
+    public float speed = 4;
+
+    public AudioClip ballAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground") {
+            AudioSource.PlayClipAtPoint(ballAudio, transform.position);
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+            //transform.position = Vector3.MoveTowards(transform.position, newPos, 3);
+
+            rig.velocity = newPos /**2*/;
+        }
+
+
+
+    }
+
+
+}
---
+++ Assets/BallRoling.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRoling : MonoBehaviour
+{
+
+    bool isGround;
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+   
+
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            audioSource.mute = true;
+        }
+    }
+
+}
